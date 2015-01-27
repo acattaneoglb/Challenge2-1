2c23095058c7c0ef7ab4c4c54a56c2c233c95f33
Ariel Cattáneo <ariel.cattaneo@globant.com>
2015-01-27 11:35:40 -0300

    Show order's text resume.

--- app/src/main/java/co/mobilemakers/sandwichstudio/MainActivity.java
+++ app/src/main/java/co/mobilemakers/sandwichstudio/MainActivity.java
@@ -7,29 +7,146 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+import java.util.EnumSet;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    final static public String KEY_BREAD = "bread";
+    final static public String KEY_TOPPINGS = "toppings";
+
+    public enum BreadEnum {
+        WHEAT,
+        WHITE,
+        RYE
+    }
+
+    public enum ToppingEnum {
+        TOMATO,
+        LATTUCE,
+        ONION,
+        CARROT,
+        SESAME,
+        OLIVES,
+        HAM,
+        CHEESE
+    }
+
     Button mButtonPlaceOrder;
+
+    RadioButton mRadioWheat, mRadioWhite, mRadioRye;
+    CheckBox mCheckTomato, mCheckLattuce, mCheckOnion, mCheckCarrot,
+        mCheckSesame, mCheckOlives, mCheckHam, mCheckCheese;
+
+    protected class OrderButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, OrderConfirmationActivity.class);
+
+            BreadEnum breadType;
+
+            if (mRadioWheat.isChecked()) {
+                breadType = BreadEnum.WHEAT;
+            }
+            else if (mRadioWhite.isChecked()) {
+                breadType = BreadEnum.WHITE;
+            }
+            else /*if (mRadioRye.isChecked())*/ {
+                breadType = BreadEnum.RYE;
+            }
+
+            EnumSet<ToppingEnum> toppingsSet = EnumSet.noneOf(ToppingEnum.class);
+
+            if (mCheckTomato.isChecked()) {
+                toppingsSet.add(ToppingEnum.TOMATO);
+            }
+            else if (mCheckLattuce.isChecked()) {
+                toppingsSet.add(ToppingEnum.LATTUCE);
+            }
+            else if (mCheckOnion.isChecked()) {
+                toppingsSet.add(ToppingEnum.ONION);
+            }
+            else if (mCheckCarrot.isChecked()) {
+                toppingsSet.add(ToppingEnum.CARROT);
+            }
+            else if (mCheckOlives.isChecked()) {
+                toppingsSet.add(ToppingEnum.OLIVES);
+            }
+            else if (mCheckSesame.isChecked()) {
+                toppingsSet.add(ToppingEnum.SESAME);
+            }
+            else if (mCheckHam.isChecked()) {
+                toppingsSet.add(ToppingEnum.HAM);
+            }
+            else if (mCheckCheese.isChecked()) {
+                toppingsSet.add(ToppingEnum.CHEESE);
+            }
+
+            intent.putExtra(KEY_BREAD, breadType);
+            intent.putExtra(KEY_TOPPINGS, toppingsSet);
+            startActivity(intent);
+        }
+    }
+
+    protected class RadioBreadListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            enableOrderIfBread();
+        }
+    }
+
+    protected void enableOrderIfBread() {
+        mButtonPlaceOrder.setEnabled(mRadioWheat.isChecked() ||
+                mRadioWhite.isChecked() || mRadioRye.isChecked());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        prepareOrderButton();
+        controlsToVars();
+        setListeners();
     }
 
-    private void prepareOrderButton() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        enableOrderIfBread();
+    }
+
+    private void controlsToVars() {
         mButtonPlaceOrder = (Button)findViewById(R.id.button_place_order);
-        mButtonPlaceOrder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, OrderConfirmationActivity.class);
-                startActivity(intent);
-            }
-        });
+
+        mRadioWheat = (RadioButton)findViewById(R.id.radio_bread_wheat);
+        mRadioWhite = (RadioButton)findViewById(R.id.radio_bread_white);
+        mRadioRye = (RadioButton)findViewById(R.id.radio_bread_rye);
+
+        mCheckTomato = (CheckBox)findViewById(R.id.check_topping_tomato);
+        mCheckLattuce = (CheckBox)findViewById(R.id.check_topping_lattuce);
+        mCheckOnion = (CheckBox)findViewById(R.id.check_topping_onion);
+        mCheckCarrot = (CheckBox)findViewById(R.id.check_topping_carrot);
+        mCheckOlives = (CheckBox)findViewById(R.id.check_topping_olives);
+        mCheckSesame = (CheckBox)findViewById(R.id.check_topping_sesame);
+        mCheckHam = (CheckBox)findViewById(R.id.check_topping_ham);
+        mCheckCheese = (CheckBox)findViewById(R.id.check_topping_cheese);
+
+        enableOrderIfBread();
+    }
+
+    private void setListeners() {
+        mButtonPlaceOrder.setOnClickListener(new OrderButtonListener());
+
+        RadioBreadListener radioListener = new RadioBreadListener();
+
+        mRadioWheat.setOnClickListener(radioListener);
+        mRadioWhite.setOnClickListener(radioListener);
+        mRadioRye.setOnClickListener(radioListener);
+
     }
 
 
--- app/src/main/java/co/mobilemakers/sandwichstudio/OrderConfirmationActivity.java
+++ app/src/main/java/co/mobilemakers/sandwichstudio/OrderConfirmationActivity.java
@@ -4,14 +4,103 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.EnumSet;
 
 
 public class OrderConfirmationActivity extends ActionBarActivity {
+
+    TextView mTextOrderResume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirmation);
+
+        controlsToVars();
+
+        String textResult = extractOrderData();
+
+        showOrderData(textResult);
+
+    }
+
+    private void showOrderData(String textResult) {
+        mTextOrderResume.setText(textResult);
+    }
+
+    private String extractOrderData() {
+        MainActivity.BreadEnum breadType;
+        EnumSet<MainActivity.ToppingEnum> toppingsList;
+
+        breadType =
+                (MainActivity.BreadEnum) getIntent().getSerializableExtra(MainActivity.KEY_BREAD);
+        toppingsList =
+                (EnumSet<MainActivity.ToppingEnum>) getIntent().getSerializableExtra(MainActivity.KEY_TOPPINGS);
+
+        String textResult = "";
+        textResult += getResources().getString(R.string.order_resume_begin);
+        textResult += getResources().getString(R.string.order_resume_bread_begin);
+        if (breadType == MainActivity.BreadEnum.WHEAT) {
+            textResult += getResources().getString(R.string.wheat);
+        }
+        else if (breadType == MainActivity.BreadEnum.WHITE) {
+            textResult += getResources().getString(R.string.white);
+        }
+        else if (breadType == MainActivity.BreadEnum.RYE) {
+            textResult += getResources().getString(R.string.rye);
+        }
+        textResult += getResources().getString(R.string.order_resume_bread_end);
+        textResult += getResources().getString(R.string.order_resume_topping_begin);
+        if (toppingsList.isEmpty()) {
+            textResult += getResources().getString(R.string.no_topping);
+        }
+        else {
+            int topsQuantity = toppingsList.size();
+            for (MainActivity.ToppingEnum topping : toppingsList) {
+                if (topping == MainActivity.ToppingEnum.TOMATO) {
+                    textResult += getResources().getString(R.string.tomato);
+                }
+                else if (topping == MainActivity.ToppingEnum.LATTUCE) {
+                    textResult += getResources().getString(R.string.lattuce);
+                }
+                else if (topping == MainActivity.ToppingEnum.ONION) {
+                    textResult += getResources().getString(R.string.onion);
+                }
+                else if (topping == MainActivity.ToppingEnum.CARROT) {
+                    textResult += getResources().getString(R.string.grated_carrot);
+                }
+                else if (topping == MainActivity.ToppingEnum.OLIVES) {
+                    textResult += getResources().getString(R.string.olives);
+                }
+                else if (topping == MainActivity.ToppingEnum.SESAME) {
+                    textResult += getResources().getString(R.string.sesame_seeds);
+                }
+                else if (topping == MainActivity.ToppingEnum.HAM) {
+                    textResult += getResources().getString(R.string.ham);
+                }
+                else if (topping == MainActivity.ToppingEnum.CHEESE) {
+                    textResult += getResources().getString(R.string.cheese);
+                }
+
+                if (topsQuantity > 2) {
+                    textResult += ", ";
+                }
+                else if (topsQuantity == 2) {
+                    textResult += getResources().getString(R.string.order_resume_topping_and);
+                }
+
+                topsQuantity--;
+            }
+        }
+
+        textResult += getResources().getString(R.string.order_resume_topping_end);
+        return textResult;
+    }
+
+    private void controlsToVars() {
+        mTextOrderResume = (TextView)findViewById(R.id.text_order_resume_text);
     }
 
 
